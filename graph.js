2f6a51e3f1240c9e614d8012bf1f46327c9bf23e
Jason VIANA <jasonv@Jasons-MacBook-Pro.local>
2021-05-23 02:02:01 +0800

    Add find paths function

--- graph.js
+++ graph.js
@@ -14,32 +14,36 @@ class Graph {
     return this.adjacencyList.has(vertex);
   }
 
+  checkVertexExistence(needExist, ...vertices) {
+    const message = needExist ? errorMessage.VERTEX_EXIST : errorMessage.VERTEX_DOES_NOT_EXIST;
+    let hasError = false;
+    vertices.forEach((v) => {
+      if (this.hasVertex(v) === needExist) {
+        hasError = true;
+        log(message, v);
+      }
+    });
+
+    return hasError;
+  }
+
   addVertex(vertex) {
-    if (this.hasVertex(vertex)) {
-      log(errorMessage.VERTEX_EXIST, vertex);
-    }
+    // Return if vertex already exist
+    if (this.checkVertexExistence(true, vertex)) return;
 
     this.adjacencyList.set(vertex, new Set());
   }
 
   addEdge(from, to) {
-    if (!this.hasVertex(from)) {
-      log(errorMessage.VERTEX_DOES_NOT_EXIST, from);
-      return;
-    }
-    if (!this.hasVertex(to)) {
-      log(errorMessage.VERTEX_DOES_NOT_EXIST, to);
-      return;
-    }
+    if (this.checkVertexExistence(false, from, to)) return;
 
     this.adjacencyList.get(from).add(to);
     this.adjacencyList.get(to).add(from);
   }
 
   removeVertex(vertex) {
-    if (!this.hasVertex(vertex)) {
-      log(errorMessage.VERTEX_DOES_NOT_EXIST, vertex);
-    }
+    if (this.checkVertexExistence(false, vertex)) return;
+
     this.adjacencyList.get(vertex).forEach((v) => {
       this.adjacencyList.get(v).delete(vertex);
     });
@@ -47,24 +51,52 @@ class Graph {
   }
 
   removeEdge(from, to) {
-    if (!this.hasVertex(from)) {
-      log(errorMessage.VERTEX_DOES_NOT_EXIST, from);
-      return;
-    }
-    if (!this.hasVertex(to)) {
-      log(errorMessage.VERTEX_DOES_NOT_EXIST, to);
-      return;
-    }
+    if (this.checkVertexExistence(false, from, to)) return;
 
     this.adjacencyList.get(from).delete(to);
     this.adjacencyList.get(to).delete(from);
   }
 
   searchVertex(vertex) {
-    return this.adjacencyList.get(vertex);
+    // return an array which are connected to vertex
+    return [...this.adjacencyList.get(vertex)];
   }
 
-  findPath(from, to) {}
+  findAllReachableVertices() {}
+
+  // Using depth-first search to find all possible paths
+  findAllPaths(from, to, visited = [], allPaths = []) {
+    // Check from and to vertices whether exist
+    if (this.checkVertexExistence(false, from, to)) return;
+
+    visited.push(from);
+
+    // Push visited path into all paths array
+    // and stop further search when reach target vertex
+    // when reach target vertex
+    if (from === to) {
+      allPaths.push([...visited]);
+      return;
+    }
+
+    // Get child vertices from current vertex
+    const destinations = this.adjacencyList.get(from);
+
+    // travel child vertex when it is not in visited array
+    for (const dest of destinations) {
+      if (visited.indexOf(dest) < 0) {
+        this.findAllPaths(dest, to, visited, allPaths);
+
+        // remove last vertex when the child vertex
+        // 1. traveled all child vertex
+        // 2. the child vertex is the target vertex
+        visited.pop();
+      }
+    }
+
+    // return all paths sorted by the path length
+    return allPaths.sort((a, b) => a.length - b.length);
+  }
 }
 
 export default Graph;
